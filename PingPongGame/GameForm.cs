e0054345f8aa4581ca612c4db1dbd1d3481ec001
ezzaansar <ezzaansar05@gmail.com>
2023-07-21 11:33:45 +0500

    Initialize varaibles

--- PingPongGame/GameForm.cs
+++ PingPongGame/GameForm.cs
@@ -13,18 +13,27 @@ namespace PingPongGame
 {
     public partial class GameForm : Form
     {
+        private const int ScreenWidth = 860;
+        private const int ScreenHeight = 540;
 
         private const int baseSpeed = 2;
         private int gameLevel = 7;
 
-        private GameItem player1;
-        private GameItem player2;
-        private Ball ball;
+        private GameItem player1 = new GameItem();
+        private GameItem player2 = new GameItem
+        {
+            Position = new Point(ScreenWidth - 3, ScreenHeight / 2)
+        };
 
-        private Random random;
+        private Ball ball = new Ball
+        {
+            Speed = new Point(2, 5)
+        };
 
-        private int player1Score;
-        private int player2Score;
+        private Random random = new Random();
+
+        private int player1Score = 0;
+        private int player2Score = 0;
 
         public GameForm()
         {
